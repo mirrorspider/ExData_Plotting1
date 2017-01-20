c8ae4c2c6afc8020c693eaf78123007a87ce8106
Alex Robinson <alexrcapgemini@gmail.com>
2017-01-20 19:10:11 +0000

    Updated comments on plot4.R

--- plot4.R
+++ plot4.R
@@ -30,34 +30,42 @@ power.data$Date <- as.Date(power.data$Date, format = "%d/%m/%Y")
 feb.power.data <- power.data[power.data$Date == "2007-02-01" | power.data$Date == "2007-02-02",]
 # Open a png device to write a png file
 png(filename = "./plot4.png", width = 480, height = 480, units = "px")
+# create a 2x2 canvas for the plots with reduced margins
 par(mfrow = c(2,2), mar = c(4,4,1,2))
+# create the four plots
+# plot 1 - Time ~ Global_active_power
 with(feb.power.data, plot(Time, Global_active_power, type="l", 
                           xlab = "", ylab = "Global Active Power (kilowatts)"))
+# plot 2 - Time ~ Voltage
 with(feb.power.data, plot(Time, Voltage, type="l", 
                           xlab = "datetime", ylab = "Voltage"))
-# draw the plot using lines and label the y axis
+# plot 3 - Time ~ sub metering
 with(feb.power.data, plot(Time, Sub_metering_1, type="l", col = "black", ylab = "Energy sub metering", xlab = ""))
 with(feb.power.data, lines(Time, Sub_metering_2, col = "red"))
 with(feb.power.data, lines(Time, Sub_metering_3, col = "blue"))
 # add a legend in the top right hand corner
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        col = c("black", "red", "blue"), lwd = "1", bty = "n")
+# plot 4 - Time ~ Global_reactive_power
 with(feb.power.data, plot(Time, Global_reactive_power, type="l", 
                           xlab = "datetime", ylab = "Global_reactive_power"))
 # close the device
 dev.off()
-# recreate the same plot to the screen to feedback to the user
+# recreate the same plots to the screen to feedback to the user
+# set up canvas
 par(mfrow = c(2,2), mar = c(4,4,1,2))
+# plot 1
 with(feb.power.data, plot(Time, Global_active_power, type="l", 
                           xlab = "", ylab = "Global Active Power (kilowatts)"))
+# plot 2
 with(feb.power.data, plot(Time, Voltage, type="l", 
                           xlab = "datetime", ylab = "Voltage"))
-# draw the plot using lines and label the y axis
+# plot 3
 with(feb.power.data, plot(Time, Sub_metering_1, type="l", col = "black", ylab = "Energy sub metering", xlab = ""))
 with(feb.power.data, lines(Time, Sub_metering_2, col = "red"))
 with(feb.power.data, lines(Time, Sub_metering_3, col = "blue"))
-# add a legend in the top right hand corner
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        col = c("black", "red", "blue"), lwd = "1", bty = "n")
+#plot 4
 with(feb.power.data, plot(Time, Global_reactive_power, type="l", 
                           xlab = "datetime", ylab = "Global_reactive_power"))
